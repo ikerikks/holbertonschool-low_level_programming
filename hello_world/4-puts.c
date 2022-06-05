5084e8cbe602e9155d2438de64b2502c42eb0b8c
ikerikks <ikerikks@gmail.com>
2022-06-06 00:54:09 +0200

    hello,puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ * main - main function
+ * Return: 0 value
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+return (0);
+
+
+}
