837fcbbe9b78944b5de3da1b4c9c5418f2b9df45
ikerikks <ikerikks@gmail.com>
2022-06-06 11:05:02 +0200

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	char bytes[] = "byte(s)";
 
 	printf("%s a char: %d %s\n", size, sizeof(char), bytes);
-	printf("%s a char: %d %s\n", size, sizeof(int), bytes);
-	printf("%s a char: %d %s\n", size, sizeof(long int), bytes);
-	printf("%s a char: %d %s\n", size, sizeof(long long int), bytes);
-	printf("%s a char: %d %s\n", size, sizeof(float), bytes);
+	printf("%s an int: %d %s\n", size, sizeof(int), bytes);
+	printf("%s a long int: %d %s\n", size, sizeof(long int), bytes);
+	printf("%s a long long int: %d %s\n", size, sizeof(long long int), bytes);
+	printf("%s a float: %d %s\n", size, sizeof(float), bytes);
 
 
 return (0);
