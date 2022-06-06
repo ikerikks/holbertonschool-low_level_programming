bd2a74f08cb3656a522d889680ccb240a1a3f0de
ikerikks <ikerikks@gmail.com>
2022-06-07 00:12:08 +0200

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 'a';
+
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+
+
+return (0);
+
+}
