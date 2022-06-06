c8fde5e38fa3ad789900bb26a1ab26eff6c8fd00
ikerikks <ikerikks@gmail.com>
2022-06-06 22:11:34 +0200

    alphabet except 'q' && 'e'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		
+		a++;
+	}
+
+	putchar('\n');
+
+
+return (0);
+
+}
