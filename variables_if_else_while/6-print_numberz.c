685c164e155d96aa04c2cfd4866df27c52ca48f9
ikerikks <ikerikks@gmail.com>
2022-06-06 22:35:56 +0200

    numbers in char values

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+
+	}
+
+	putchar('\n');
+
+
+return (0);
+
+}
