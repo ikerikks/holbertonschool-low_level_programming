ceb0de21bd088f5236d69d2eeee2d1c776104b76
ikerikks <ikerikks@gmail.com>
2022-06-07 18:43:34 +0200

    alphabet function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
+
+
+}
+
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+
+}
+
