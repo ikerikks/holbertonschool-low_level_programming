c7942551d633b12e43dcb5f523b8307ca99cc129
ikerikks <ikerikks@gmail.com>
2022-06-08 19:11:31 +0200

    add function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: integer
+ * @num2: integer
+ * Return: the sum value of two integers
+ */
+
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+
+}
