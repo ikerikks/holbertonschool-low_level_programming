a4146c74992a101272428effebbf1f68d6ab03c7
ikerikks <ikerikks@gmail.com>
2022-06-10 14:43:48 +0200

    times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * times_table - prints the 9 times table
  *
- * Return: any value
+ * Return - any value
  */
-
 
 void times_table(void)
 {
@@ -17,7 +15,6 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-
 			if (j != 0)
 			{
 				_putchar(',');
@@ -31,10 +28,7 @@ void times_table(void)
 				{
 					_putchar(' ');
 					_putchar(' ');
-
 				}
-
-
 			}
 
 			if ((i * j) > 9)
@@ -48,9 +42,10 @@ void times_table(void)
 				_putchar((i * j) % 10 + '0');
 			}
 
+			_putchar('\n');
 
 		}
 
-		_putchar('\n');
 	}
+
 }
