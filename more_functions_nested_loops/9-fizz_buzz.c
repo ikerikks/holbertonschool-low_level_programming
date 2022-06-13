31e4a5b470a7be4bf27257b65707880e3f6eec69
ikerikks <ikerikks@gmail.com>
2022-06-13 15:23:02 +0200

    The Fizz-Buzz test program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point (main function)
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz"); 
+		}
+
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz"); 
+		}
+
+		else 
+		{
+			printf("%d", i);
+		}
+
+		putchar(' '); 
+
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
