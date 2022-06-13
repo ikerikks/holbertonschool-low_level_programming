0061d809b27d3b0c74073b6857c83e4cd2f2d503
ikerikks <ikerikks@gmail.com>
2022-06-13 15:30:19 +0200

    update of The Fizz-Buzz test program

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz"); 
+			printf("FizzBuzz");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
 		}
 
 		else if (i % 5 == 0)
@@ -22,17 +27,12 @@ int main(void)
 			printf("Buzz");
 		}
 
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz"); 
-		}
-
-		else 
+		else
 		{
 			printf("%d", i);
 		}
 
-		putchar(' '); 
+		putchar(' ');
 
 	}
 
