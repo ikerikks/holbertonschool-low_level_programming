4a245508d74a87ae549e7697d0a05fc28c885534
ikerikks <ikerikks@gmail.com>
2022-06-15 12:05:02 +0200

    update of print_rev function

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: pointer parameter that points to the adress of a variable of type char
+ *
+ * Return: any value
+ */
+
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+
+
+}
