dd66ab8ca7de294ffd804251e46266d98c88b0f1
ikerikks <ikerikks@gmail.com>
2022-06-15 19:47:12 +0200

    update of  _strcpy function

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,20 +13,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int n;
 	int i;
-
-	for (n = 0; src[n] != '\0'; n++)
-	;
 
 	for (i = 0; src[i]; i++)
 	{
 		dest[i] = src[i];
-	}
-
-	for ( ; i < n; i++)
-	{
-		dest[i] = '\0';
 	}
 
 	return (dest);
