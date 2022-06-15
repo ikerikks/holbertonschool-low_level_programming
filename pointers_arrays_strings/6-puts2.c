a4ac5a87447841f8da19ae9881a9abb793bee44e
ikerikks <ikerikks@gmail.com>
2022-06-15 17:22:47 +0200

    update of puts2 function

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,14 @@
 void puts2(char *str)
 {
 	int i;
+	int j;
 
-	for (i = 0; str[i] != '\0'; str += 2)
+	for (i = 0; str[i] != '\0'; i++)
+	;
+
+	for (j = 0; j <= i; j += 2)
 	{
-		_putchar(str[i]);
+		_putchar(str[j]);
 
 	}
 
