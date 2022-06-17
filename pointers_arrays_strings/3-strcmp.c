dfd7712c77a55bc602e57825374c10735438a29f
ikerikks <ikerikks@gmail.com>
2022-06-17 09:25:49 +0200

    _strcmp, function that compares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: pointer parameter of type char
+ *
+ * @s2: pointer parameter of type char
+ *
+ * Return: integer value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	
+	if (i == j)
+	{
+		return (0);
+	}
+
+	else if (i < j)
+	{
+		return (-15);
+	}
+
+	else
+	{
+		return (15);
+	}
+	
+}
