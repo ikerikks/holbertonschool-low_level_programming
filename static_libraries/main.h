e3f8849ebd109be5bb4afd20a1dede83c32fc384
ikerikks <ikerikks@gmail.com>
2022-06-21 11:54:39 +0200

    correction of main header file because left of double inclusion

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 /* Functions, nested loops */
 
 int _putchar(char c);
@@ -61,3 +64,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+
+#endif /* _MAIN_H_ */
