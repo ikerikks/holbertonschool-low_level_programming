21fb6f33bc5402d2cc0dbbe201730d5fd045164d
ikerikks <ikerikks@gmail.com>
2022-06-21 13:28:20 +0200

    c program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ *
+ * @argc: counter argument
+ *
+ * @argv: vector argument
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", *argv);
+
+
+	return (0);
+
+
+}
