9835d12c6fe69481a129b88abe98efa1802c5f78
ikerikks <ikerikks@gmail.com>
2022-06-22 12:57:22 +0200

    c program that adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -22,25 +22,22 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
+		if (argc == 1)
+		{
+			printf("0\n");
+		}
+
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argc == 1)
-			{
-				printf("0\n");
-			}
-
-			else if (!isdigit(argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 
 				return (1);
 			}
-
-			else
-				sum += atoi(argv[i]);
-
 		}
 
+		sum += atoi(argv[i]);
 
 	}
 
