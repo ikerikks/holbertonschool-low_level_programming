c2a9c6843529425dbc6600d66c44fe18d5049b23
ikerikks <ikerikks@gmail.com>
2022-06-23 13:05:36 +0200

    _strdup, function that duplicates a pointer

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer copied by a string in a new allocated memory
+ *
+ * @str: string
+ *
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+	int size;
+
+	for (i = 0; str[i] != '\0'; i++)
+	;
+
+	size = i;
+
+	s = malloc(size * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = str[i];
+
+	return (s);
+
+
+}
