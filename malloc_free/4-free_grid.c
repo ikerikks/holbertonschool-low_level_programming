0939ab17e937609346b6f20ddf8e99d242ab9332
ikerikks <ikerikks@gmail.com>
2022-06-24 21:36:55 +0200

    free_grid, function that frees a 2 dimensional array

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional array of integers
+ *
+ * @grid: pointer to pointer
+ *
+ * @height: height
+ *
+ * Return: pointer
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+
+
+	
+
+}
+
