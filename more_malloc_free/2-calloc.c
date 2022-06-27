d6cb9b54d05f395d21de096ee730feafd48d53c8
ikerikks <ikerikks@gmail.com>
2022-06-27 19:40:47 +0200

    update of_calloc function

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,6 +14,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,6 +23,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
 
 	
 	return (ptr);
