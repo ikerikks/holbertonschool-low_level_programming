0dde42865bb5d09ad0a6e2169f8d69612f87ab59
ikerikks <ikerikks@gmail.com>
2022-06-28 15:34:37 +0200

    update of string_nconcat function : adding of a condition

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -36,15 +36,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 
 	s3 = malloc((size1 + n + 1) * sizeof(char));
+	
+	if (n >= size2)
+		s3 = malloc((size1 + size2 + 1) * sizeof(char));
 
 	if (s3 == NULL)
 	{
 		return (NULL);
 		free(s3);
 	}
-
-	if (n > size2)
-		n = size2;
 
 	for (i = 0; i < size1; i++)
 		s3[i] = s1[i];
