9d6196ead25b4d616ed41d211e4d87860c84697a
ikerikks <ikerikks@gmail.com>
2022-06-28 23:01:25 +0200

    update of betty documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef __DOG__
 #define __DOG__
 
-struct dog {
+/**
+ * struct dog - new type of structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
