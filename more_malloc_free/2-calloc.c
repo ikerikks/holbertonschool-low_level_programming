0923bf392935cc56b41a9ea996f4b6ce41247b3d
ikerikks <ikerikks@gmail.com>
2022-06-28 14:32:48 +0200

    betty syntax correction

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr= malloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 		return (NULL);
@@ -27,7 +27,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
 
-	
+
 	return (ptr);
 
 }
