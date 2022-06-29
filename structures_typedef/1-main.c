5f3f06c07110755c75ffbf85004f9e57f246519d
ikerikks <ikerikks@gmail.com>
2022-06-29 11:26:32 +0200

    update of init_dog function

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -9,10 +9,8 @@
 
 int main(void)
 {
-    struct dog my_dog;
+	init_dog(NULL, "Ghost", 4.75, "Jon Snow");
+	printf("Ok\n");
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    
-    return (0);
+	return (0);
 }
