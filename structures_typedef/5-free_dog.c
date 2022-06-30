f2e66e81b02809a7a6fe5f71f220cde29a826059
ikerikks <ikerikks@gmail.com>
2022-06-30 20:23:24 +0200

    free_dog, function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer of type structure
+ *
+ * Return: struct type
+ */
+
+void free_dog(dog_t *d)
+{
+
+	if (d != NULL)
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
+
+
+}
