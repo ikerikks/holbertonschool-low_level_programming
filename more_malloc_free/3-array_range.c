02d10d612475cc7e5e29c7acad4910c2349bc66c
ikerikks <ikerikks@gmail.com>
2022-06-30 09:52:10 +0200

    update of array_range function)

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,14 +17,13 @@ int *array_range(int min, int max)
 	if ( min > max)
 		return (NULL);
 
-	ptr = malloc(max - min + 1);
+	ptr = malloc(sizeof(int) * (max - min + 1));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	else
-		for (i = min; i < max; i++)
-			ptr[0] = min;
+	for (i = 0; i <= max; min++, i++)
+		ptr[i] = min;
 
 	return (ptr);
 
