5e27616326e69bb8492886ceb2372ba69622672e
ikerikks <ikerikks@gmail.com>
2022-07-02 00:04:45 +0200

    update of betty syntax

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,25 +10,25 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	  op_t ops[] = {
-		  {"+", op_add},
-		  {"-", op_sub},
-		  {"*", op_mul},
-		  {"/", op_div},
-		  {"%", op_mod},
-		  {NULL, NULL}
-	  };
-	  int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-	  while (ops[i].op != NULL)
-	  {
-		  if (s[0] == ops[i].op[0])
-			  return (ops[i].f);
+	while (ops[i].op != NULL)
+	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
 
-		  i++;
-	  }
+		i++;
+	}
 
-	  return (NULL);
+	return (NULL);
 
 
 }
