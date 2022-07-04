b5a5d99693b42fe392b16cb4ab6359f368f96549
ikerikks <ikerikks@gmail.com>
2022-07-04 19:26:41 +0200

    update of print_strings function

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,15 +15,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	char *str = va_arg(ap, char *);
 
 	va_start(ap, n);
 
 
 	for (i = 0; i < n; i++)
 	{
-		if (str == NULL)
-			str = "(nil)";
 
 		printf("%s", va_arg(ap, char *));
 
