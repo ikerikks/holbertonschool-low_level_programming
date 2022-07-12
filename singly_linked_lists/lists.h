70ff125d904db022ecab99ea4b9c087146639159
ikerikks <ikerikks@gmail.com>
2022-07-12 15:02:11 +0200

    header file

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef __LISTS__
+#define __LISTS__
+
+struct list_t;
+
+/* Prototypes */
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
