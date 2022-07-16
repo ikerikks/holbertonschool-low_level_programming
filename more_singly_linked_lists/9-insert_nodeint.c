81e09b290b2ed8752de0e0222c65e6d82647b432
ikerikks <ikerikks@gmail.com>
2022-07-16 23:50:40 +0200

    correction of betty style

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: HEAD node of link list
  * @idx: index
  * @n: node data
@@ -19,15 +19,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = *head;
 	if (idx != 0)
 	{
-		for (i = 0; i < idx -1 && head != NULL; i++)
+		for (i = 0; i < idx - 1 && head != NULL; i++)
 		{
 			temp = temp->next;
 		}
 	}
 
-	if(temp == NULL && idx != 0)
+	if (temp == NULL && idx != 0)
 		return (NULL);
-	
+
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
