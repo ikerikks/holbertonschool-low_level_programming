7e269a91bf715b6d0842b8e5573e7ffcd876d78b
ikerikks <ikerikks@gmail.com>
2022-07-22 11:49:19 +0200

    second correction opf function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,9 +23,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+		text_content = "";
 
-	if (text_content)
+	file = open(filename, O_CREAT | O_TRUNC |O_WRONLY | O_APPEND);
+
+	if (text_content != NULL)
 	{
 		for (letters = 0; text_content[letters]; letters++)
 			;
