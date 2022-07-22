43ed6a58925a36cb24546768ff093144295983e2
ikerikks <ikerikks@gmail.com>
2022-07-22 10:58:51 +0200

    correction of the function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,39 +6,38 @@
 /**
  * create_file - creates a file
  * @filename: nameof file
- * @letters: number of letters
+ * @text_content: content of the file
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
 
 	int file;
-	int wr;
 	int letters;
+	int wr;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	file = open(filename, O_WRONLY | O_APPEND);
 
-	if (file == -1)
-		return (-1);
-
-	if (text_content == NULL)
+	if (text_content)
 	{
 		for (letters = 0; text_content[letters]; letters++)
 			;
 
 		wr = write(file, text_content, letters);
-
-		if (wr == -1)
+		if (wr == (-1))
 			return (-1);
+
 	}
-	
+
 	close(file);
 
-	return(1);
+	return (1);
 
 }
