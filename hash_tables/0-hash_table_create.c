bf274d5b3fa2cd6d41b12377bfe67f3307d33e78
ikerikks <ikerikks@gmail.com>
2022-09-02 16:30:04 +0200

    task1: betty style correction

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: size
+ *
+ * Return: pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
